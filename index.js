2f758e0bf947ca796cc0136119c97e4f82d8fcf6
Gloria Lau <glorialau@live.ca>
2024-06-23 12:43:56 -0700

    add firebase functions

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const { PORT } = process.env;
+// const { OUT_PORT } = process.env;
 
 const topicRoutes = require("./routes/questionsroute");
 const chatbotRoutes = require("./routes/chatbot");
+const functions = require("firebase-functions");
 
 //Middleware
 app.use(cors());
@@ -17,6 +18,8 @@ app.use("/topic", topicRoutes);
 app.use("/chatbot", chatbotRoutes);
 
 //Listening
-app.listen(PORT, () => {
-  console.log("Running on port:", PORT);
-});
+// app.listen(OUT_PORT, () => {
+//   console.log("Running on port:", OUT_PORT);
+// });
+
+exports.api = functions.https.onRequest(app);
